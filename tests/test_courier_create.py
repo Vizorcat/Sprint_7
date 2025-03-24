bd7a405a7475eaeecdd5ff950c7d16a1e67407d1
Artem Pavlov <vizorcat@yandex.ru>
2025-03-24 15:41:46 +0300

    Вынес фикстуру в отдельный файл conftest.py

--- tests/test_courier_create.py
+++ tests/test_courier_create.py
@@ -6,23 +6,6 @@ from config import Urls
 from helpers import create_random_login, create_random_password, create_random_firstname
 
 
-@pytest.fixture
-def courier_teardown():
-
-    payload = {
-        'login': create_random_login(),
-        'password': create_random_password(),
-        'firstName': create_random_firstname()
-    }
-    response = requests.post(Urls.URL_courier_create, json=payload)
-    assert response.status_code == 201, f"Failed to create courier: {response.json()}"
-    courier_id = response.json().get('id')
-
-    yield payload, courier_id
-
-    if courier_id:
-        delete_response = requests.delete(f"{Urls.URL_courier_delete}/{courier_id}")
-        assert delete_response.status_code == 200, f"Failed to delete courier: {delete_response.json()}"
 
 class TestCourierCreate:
 
